034f7961fd017b259a47043134a03abdcf4f702c
nicas83 <gaetano.nicassio@gmail.com>
2023-11-26 12:14:09 +0100

    fixed api parameter

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def get_all_kpi():
         return jsonify({"error": "An error occurred retrieving the KPIs."}), 500
 
 
-@app.route('/kpis/{kpi_name}/stats', methods=['GET'])
+@app.route('/kpis/stats/<kpi_name>', methods=['GET'])
 def get_kpi_stats(kpi_name):
     kpis = None
     if kpi_name == '' or kpi_name.upper() == 'ALL':
